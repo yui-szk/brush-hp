e547fc67fc2956d758984486b0ebdc868dfa2a8e
yui-szk <yuisuzuki.017@gmail.com>
2024-02-08 03:37:36 +0900

    pageにスタイル追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Header } from "@/components/header";
 
 export default function Page() {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col">
       <Header />
     </div>
   );
