7631b86c12cf74440b4f6ac2c403bc100cb239cd
yui-szk <yuisuzuki.017@gmail.com>
2024-02-08 03:35:19 +0900

    footer追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
 
 export default function Page() {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,8 @@
+export function Footer() {
+  return (
+    <footer className="mt-auto flex h-20 items-center justify-center bg-slate-400">
+      <p>&copy; 2024 Brush</p>
+      <img className="m-3 w-6" src="/Instagram.svg" alt="Instagram icon" />
+    </footer>
+  );
+}
