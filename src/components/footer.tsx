7b0ad18d0f21e47d6fe8cc20c10854553c34c1dc
yui-szk <yuisuzuki.017@gmail.com>
2024-02-08 04:04:43 +0900

    imgタグ->Imageコンポーネント

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image";
+
 export function Footer() {
   return (
     <footer className="mt-auto flex h-20 items-center justify-center bg-slate-400">
       <p>&copy; 2024 Brush</p>
-      <img className="m-3 w-6" src="/Instagram.svg" alt="Instagram icon" />
+      <Image
+        className="m-3"
+        src="/Instagram.svg"
+        alt="Instagram icon"
+        width={28}
+        height={28}
+      />
     </footer>
   );
 }
