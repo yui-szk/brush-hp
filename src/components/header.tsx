505daf45325046796edec0f71c9703b29619950e
yui-szk <yuisuzuki.017@gmail.com>
2024-02-08 02:31:38 +0900

    title部分をmapに変更

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,24 +1,35 @@
+import Link from "next/link";
+
 export function Header() {
+  const values = [
+    { name: "About us", href: "/" },
+    { name: "News", href: "/" },
+    { name: "Member", href: "/" },
+    { name: "Works", href: "/" },
+    { name: "Contact", href: "/" },
+  ];
+
   return (
-    <div className="flex h-24 justify-between px-14 py-9">
-      <p className="px-4 text-2xl">Brush</p>
-      <li className="flex justify-end pl-4">
-        <a className="mx-7" target="_self">
-          About us
-        </a>
-        <a className="mx-7" target="_self">
-          news
-        </a>
-        <a className="mx-7" target="_self">
-          Members
-        </a>
-        <a className="mx-7" target="_self">
-          Works
-        </a>
-        <a className="mx-7" target="_self">
-          Contact
-        </a>
-      </li>
-    </div>
+    <header className="flex h-24 items-center justify-between bg-slate-300 px-14 py-9">
+      <Link href="/" className="cursor-pointer px-2 text-2xl">
+        Brush
+      </Link>
+      <input
+        id="menu-button"
+        type="checkbox"
+        className="peer fixed right-1 top-0 z-10 h-16 w-16 cursor-pointer appearance-none before:absolute before:right-5 before:top-8 before:block before:h-0.5 before:w-6 before:-translate-y-1 before:bg-black before:duration-300 after:absolute after:right-5 after:top-8 after:block after:h-0.5 after:w-6 after:translate-y-1 after:bg-black after:duration-300 checked:before:translate-y-0 checked:before:rotate-45 checked:after:translate-y-0 checked:after:-rotate-45 lg:left-6 lg:hidden"
+      />
+      <nav>
+        <ul className="flex justify-end pl-4">
+          {values.map(({ name, href }) => (
+            <li key={name}>
+              <Link href={href} className="mx-7 cursor-pointer" target="_self">
+                {name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
   );
 }
