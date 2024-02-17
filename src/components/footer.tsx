143c60ae5dd19b562640d5fb6fbe9e1baefccc44
yui-szk <yuisuzuki.017@gmail.com>
2024-02-17 23:14:58 +0900

    Linkコンポーネントを追加

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,13 +5,15 @@ export function Footer() {
   return (
     <footer className="mt-auto flex h-20 items-center justify-center bg-slate-400">
       <p>&copy; 2024 Brush</p>
-      <Image
-        className="m-3"
-        src="/Instagram.svg"
-        alt="Instagram icon"
-        width={28}
-        height={28}
-      />
+      <Link href="https://www.instagram.com/brush_myu/">
+        <Image
+          className="m-3"
+          src="/Instagram.svg"
+          alt="Instagram icon"
+          width={28}
+          height={28}
+        />
+      </Link>
     </footer>
   );
 }
