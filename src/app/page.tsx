e22175297ea350d7c00fc7abe21b91f61a5b3969
yui-szk <yuisuzuki.017@gmail.com>
2024-02-17 23:15:26 +0900

    Header, Footerコンポーネントをlayout.tsxに移動

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import { Header } from "@/components/header";
-import { Footer } from "@/components/footer";
-
 export default function Page() {
   return (
     <div className="flex min-h-screen flex-col">
-      <Header />
       <main className="max-w-none"></main>
-      <Footer />
     </div>
   );
 }
